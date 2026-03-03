e579360755216d5754404744c464ce22fdca81fb
Rafael Missio <rtmissio@gmail.com>
2026-03-03 00:04:10 -0300

    adcionando a BaseTest

---
+++ src/test/java/base/BaseTest.java
@@ -0,0 +1,13 @@
+package base;
+
+import io.restassured.RestAssured;
+import org.junit.jupiter.api.BeforeAll;
+
+
+public class BaseTest {
+
+    @BeforeAll
+    public static void setup() {
+        RestAssured.baseURI = "https://dog.ceo/api";
+    }
+}
